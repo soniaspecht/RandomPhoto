9b1720438be3d2f1acb64950bde0eb03f787e242
Sonia Specht <soniaspechtdelatorre@gmail.com>
2025-10-05 18:47:49 +0200

    SimboloGiranod

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import Botoncito from './components/botoncito'
-import './App.css'
+import { useState } from 'react';
+import Botoncito from './components/botoncito';
+import './App.css';
 
 function App() {
   const [url, setUrl] = useState("");
 
+  // Servirá para la acción de cargar
+  const [load, isLoading] = useState(false);
+
+  // Para actualizar la imagen y la acción de cargar
+  const updatePhoto = async (randomImgUrl) => {
+    // La rueda de cargar comenzará
+    isLoading(true);
+    // Se intenta convertir la imagen 
+    try {
+      // Se obtiene la imagen , se tranforma en datos binarios y luego a b64
+      const res = await fetch(randomImgUrl);
+      const binary = await res.blob();
+      const reader = new FileReader();
+      // Se actualiza la imagen y se quitar la rueda de cargar
+      reader.onloadend = () => {
+        setUrl(reader.result);
+        isLoading(false);
+      };
+      reader.readAsDataURL(binary);
+    } catch {
+      // Si no funciona, se pone la imagen direcatmanete y se elimina la rueda de cargar
+      setUrl(randomImgUrl);
+      isLoading(false);
+    }
+  };
+ 
+
   return (
     <div className="app">
-      <img
-        className="randomImg"
-        src={url || `https://picsum.photos/1200/800?blur=1&random=${Math.floor(Math.random()*1e6)}`}
-        alt="Random"
-      />
+
+      <div className="imageContainer">
+        <img
+          className="randomImg"
+          src={url || `https://picsum.photos/1200/800?blur=1&random=${Math.floor(Math.random()*1e6)}`}
+          alt="Random"
+          // Para ocultar o mostrar el simbolo de cargar
+          style={{ visibility: load ? 'hidden' : 'visible' }}
+        />
+        {load && <div className="loadSymbol"></div>}
+      </div>
 
       <div className="buttonContainer">
         <Botoncito
           emoji="👤"
           // La url por defecto es de thispersondoesnotexist.com
-          onRandomImgFetch={(randomImgUrl) => {
-            setUrl(randomImgUrl);
-          }}
+          randomImgFunction={async () => "https://thispersondoesnotexist.com/#nonce=" + Math.floor(Math.random()*1e10)}
+          onRandomImgFetch={updatePhoto}
         />
 
+        
         <Botoncito
           emoji="🌿"
           randomImgFunction={async () => {
             return `https://picsum.photos/seed/${Math.floor(Math.random()*1e9)}/1200/800`;
           }}
-          onRandomImgFetch={(randomImgUrl) => {
-            setUrl(randomImgUrl);
-          }}
+          onRandomImgFetch={updatePhoto}
         />
 
         <Botoncito
@@ -39,9 +70,7 @@ function App() {
             const data = await res.json();
             return data.image;
           }}
-          onRandomImgFetch={(randomImgUrl) => {
-            setUrl(randomImgUrl);
-          }}
+          onRandomImgFetch={updatePhoto}
         />
 
         <Botoncito
@@ -51,9 +80,7 @@ function App() {
             const data = await res.json();
             return data.message;
           }}
-          onRandomImgFetch={(randomImgUrl) => {
-            setUrl(randomImgUrl);
-          }}
+          onRandomImgFetch={updatePhoto}
         />
 
         <Botoncito
@@ -62,9 +89,7 @@ function App() {
             const isGif = Math.random() < 0.5;
             return "https://cataas.com/cat" + (isGif ? "/gif" : "") + "#nonce=" + Math.floor(Math.random()*1e10); // Nonce usado para no tirar de caché de la imagen
           }}
-          onRandomImgFetch={(randomImgUrl) => {
-            setUrl(randomImgUrl);
-          }}
+          onRandomImgFetch={updatePhoto}
         />
 
         <Botoncito
@@ -72,13 +97,12 @@ function App() {
           randomImgFunction={async () => {
             return "https://placebear.com/200/300#nonce=" + Math.floor(Math.random()*1e10); // Nonce usado para no tirar de caché de la imagen
           }}
-          onRandomImgFetch={(randomImgUrl) => {
-            setUrl(randomImgUrl);
-          }}
+          onRandomImgFetch={updatePhoto}
         />
+
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
